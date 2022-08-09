c0274faafa2213b305e229acfe73332b8624a719
cutexxx <cutexxx@163.com>
2022-08-09 14:21:08 +0800

    m1

--- dict.py
+++ dict.py
@@ -1,3 +1,22 @@
+studentIds = {'knuth': 42.0, 'turing': 56.0, 'nash': 92.0}
+print(studentIds['turing'])
+
+studentIds['nash'] = 'ninety-two'
+print(studentIds)
+
+del studentIds['knuth']
+print(studentIds)
+
+studentIds['knuth'] = [42.0, 'forty-two']
+print(studentIds)
+
+print(studentIds.keys())
+
+print(studentIds.values())
+print(studentIds.items())
+print(len(studentIds))
+
+
 fruits = ['apples', 'oranges', 'pears', 'bananas']
 for fruit in fruits:
     print(fruit + ' for sale')
